23c1c433b35719ec3ca31be123d4acd42e664e39
Raditya Mulya Akbar <radityamulyaakbar2@gmail.com>
2024-09-04 14:17:55 +0800

    char

--- src/Character.java
+++ src/Character.java
@@ -1,2 +1,11 @@
 public class Character {
+    public static void main(String[] args) {
+        char letter = '\u0041';  // Unicode for character 'A'
+        char number = '\u0031';  // Unicode for character '1'
+        char symbol = '\u0040';  // Unicode for character '@'
+
+        System.out.println("Letter: " + letter);
+        System.out.println("Number: " + number);
+        System.out.println("Symbol: " + symbol);
+    }
 }
