89ad0db981c229ab3d6778b4fc09d8c0714155fc
Raditya Mulya Akbar <radityamulyaakbar2@gmail.com>
2024-09-06 13:24:38 +0800

    boolean

---
+++ src/BooleanExample.java
@@ -0,0 +1,29 @@
+public class BooleanExample {
+    public static void main(String[] args) {
+        // Boolean variable declaration and initialization
+        boolean isJavaFun = true;   // A boolean value set to true
+        boolean isFishTasty = false; // A boolean value set to false
+
+        // Output the boolean values
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is fish tasty? " + isFishTasty);
+
+        // Using booleans in conditional statements
+        if (isJavaFun) {
+            System.out.println("Yes, Java is fun!");
+        } else {
+            System.out.println("No, Java is not fun.");
+        }
+
+        // Example of a boolean expression
+        int x = 10;
+        int y = 20;
+        boolean comparisonResult = x < y;  // true because 10 is less than 20
+
+        System.out.println("Is x less than y? " + comparisonResult);
+
+        // Using the not (!) operator
+        boolean reverseResult = !comparisonResult;  // false because !true is false
+        System.out.println("Reverse of comparisonResult: " + reverseResult);
+    }
+}
--- src/TypeDataNumber.java
+++ src/TypeDataNumber.java
@@ -1,17 +1,23 @@
 public class TypeDataNumber {
     public static void main(String[] args) {
         // Tipe data bilangan bulat
-        byte iniByte = 100;
-        short iniShort = 1000;
-        int iniInt = 100000000;
-        long iniLong = 1000000000;
-        long iniLong2 = 100000000000L;
+        byte exampleByte = 100;
+        short exampleShort = 1000;
+        int exampleInt = 100_000_000;
+        long exampleLong1 = 1_000_000_000L;
+        long exampleLong2 = 100_000_000_000L;
 
         // Floating point
-        float iniFloat = 1.5F;
-        double iniDouble = 12.2404;
+        float exampleFloat = 1.5F;
+        double exampleDouble = 12.2404;
 
-
-
+        // Menampilkan hasil
+        System.out.println("Byte value: " + exampleByte);
+        System.out.println("Short value: " + exampleShort);
+        System.out.println("Int value: " + exampleInt);
+        System.out.println("Long value 1: " + exampleLong1);
+        System.out.println("Long value 2: " + exampleLong2);
+        System.out.println("Float value: " + exampleFloat);
+        System.out.println("Double value: " + exampleDouble);
     }
 }
